4550ba85790cb98e0b352e2566c1948c2dfac10f
SpiderFB <arnab9954@gmail.com>
2024-06-29 03:14:43 +0530

    More fix

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import os
 import win32com.client
-# from win32com.client import constants
 import pandas as pd
-# from openpyxl import load_workbook
+# import openpyxl
 
 excel_app = win32com.client.Dispatch("Excel.Application")
 
@@ -58,6 +57,7 @@ fun_cp(ATLAS)
 
 
 
+
 # Load the two sheets into dataframes
 df1 = pd.read_excel('C:/Users/2095421/Downloads/Migration/MBEW_DataValidation.xlsx', sheet_name='MBEW_M1Q_Ext.xlsx')
 df2 = pd.read_excel('C:/Users/2095421/Downloads/Migration/MBEW_DataValidation.xlsx', sheet_name='MBEW_APB_Ext.xlsx')
@@ -66,32 +66,36 @@ df2 = pd.read_excel('C:/Users/2095421/Downloads/Migration/MBEW_DataValidation.xl
 df1.set_index('KEY_MBEW_M1Q_Ext.xlsx', inplace=True)
 df2.set_index('KEY_MBEW_APB_Ext.xlsx', inplace=True)
 
-# Get the column of interest
-col_name = 'PEINH'
 
-result_df = pd.DataFrame(columns=['Key','Value-Matched?','Material','Plant', 'ATLAS', 'MDG'])
-# print(type(result_df))
-
-for index in df2.index:
-    # Check if the index is in df2
-    if index in df1.index:
-        # Check if the values are the same
-        if df2.loc[index, col_name] == df1.loc[index, col_name]:
-            print(f'Key {index}: Yes')
-            # result_df = result_df.append({'Key': index, 'Value': 'Yes'}, ignore_index=True)
-            result_df.loc[len(result_df)] = [index, 'Yes',df2.loc[index, 'MATNR'],df2.loc[index, 'BWKEY'], df1.loc[index, col_name], df1.loc[index, col_name]]
-        else:
-            print(f'Key {index}: No')
-            # result_df = result_df.append({'Key': index, 'Value': 'No'}, ignore_index=True)
-            result_df.loc[len(result_df)] = [index, 'No',df2.loc[index, 'MATNR'],df2.loc[index, 'BWKEY'], df1.loc[index, col_name], df1.loc[index, col_name]]
-    else:
-        print(f'Key {index}: Key not found')
-        # result_df = result_df.append({'Key': index, 'Value': 'Key Not Found'}, ignore_index=True)
-        # result_df.loc[len(result_df)] = [index, 'Key Not Found',df2.loc[index, 'MATNR'],df2.loc[index, 'BWKEY']]
-        result_df.loc[len(result_df)] = [index, 'Key Not Found','','','','']
+# Get the column of names in iteration
+df3 = pd.read_excel('C:/Users/2095421/Downloads/Migration/MBEW_DataValidation.xlsx', sheet_name='Sheet1')
+df_scope = df3[df3['Comment'] == 'Scope']
+scope_field = df_scope['Technical Name']
 
 
 
-writer = pd.ExcelWriter('C:/Users/2095421/Downloads/Migration/MBEW_DataValidation.xlsx', engine='openpyxl', mode='a')
-result_df.to_excel(writer, sheet_name=col_name)
-writer.close()
+for col_name in scope_field:
+    print(col_name)
+    if col_name != 'MATNR' and col_name != 'BWKEY' :
+
+        # col_name = 'PEINH'
+        result_df = pd.DataFrame(columns=['Key','Value-Matched?','Material','Plant', 'ATLAS', 'MDG'])
+        # print(type(result_df))
+
+        for index in df2.index:
+            # Check if the index is in df2
+            if index in df1.index:
+                # Check if the values are the same
+                if df2.loc[index, col_name] == df1.loc[index, col_name] or (pd.isna(df2.loc[index, col_name]) and pd.isna(df1.loc[index, col_name])):
+                    print(f'Key {index}: Yes')
+                else:
+                    print(f'Key {index}: No')
+                    result_df.loc[len(result_df)] = [index, 'No',df2.loc[index, 'MATNR'],df2.loc[index, 'BWKEY'], df1.loc[index, col_name], df1.loc[index, col_name]]
+            else:
+                print(f'Key {index}: Key not found')
+                result_df.loc[len(result_df)] = [index, 'Key Not Found','','','','']
+
+        if not result_df.empty:
+            writer = pd.ExcelWriter('C:/Users/2095421/Downloads/Migration/MBEW_DataValidation.xlsx', engine='openpyxl', mode='a')
+            result_df.to_excel(writer, sheet_name=col_name)
+            writer.close()
